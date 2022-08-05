7589bc49d9e01de7c9e765bbb3b05bd759b06e68
subra419 <subramanian419@gmail.com>
2022-08-05 21:11:11 +0530

    mytask

---
+++ src/org/git/Prog.java
@@ -0,0 +1,11 @@
+package org.git;
+
+public class Prog {
+	public static void main(String[] args) {
+		int i, fact=1 ;
+		for(i=1;i<=5;i++) {
+			fact=fact*i;
+			}
+			System.out.println("factorial " +fact);
+			}
+}
---
+++ src/org/git/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Acer
+ *
+ */
+package org.git;
